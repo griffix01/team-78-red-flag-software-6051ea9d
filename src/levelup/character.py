cf8a46c77a57d3821ad3b91fb2a55bb6da8d06b6
Doug Griffith <doug.griffith01@gmail.com>
2023-09-21 21:07:46 +0000

    finally made progress

--- src/levelup/character.py
+++ src/levelup/character.py
@@ -1,14 +1,17 @@
 from levelup.position import Position
+from levelup.map import Map
 
 class Character:
     name = ""
     position: Position
+    map: Map
 
     def __init__(self, character_name):
         self.name = character_name
 
     def enterMap(self, map): # set start location
+        self.map = map
         self.position = Position(0, 0)
 
     def move(self, direction):
-        pass
+        self.position = self.map.calculatePosition(self.position, direction)
--- tests/test_character.py
+++ tests/test_character.py
@@ -22,10 +22,6 @@ class TestMove(TestCase):
         testobj = Character("name")
         testmap = fakeMap(0,0,9,9)
         testobj.enterMap(testmap)
-        #testGameStatus = GameStatus(testobj.name)
         testobj.move("w")
         self.assertEqual(testobj.position.x, 0)
         self.assertEqual(testobj.position.y, 1)
-        #self.assertEqual(testGameStatus.moveCount, 1)
-
-
